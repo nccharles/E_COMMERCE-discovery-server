cebca459004e7017def8c2a2376ccbc81be5ee05
Charles NDAYISABA <nccharles1@gmail.com>
2024-10-28 10:36:51 +0200

    Fixing Outh2 issue

--- src/test/java/com/amalitech/eurekaserver/EurekaServerApplicationTests.java
+++ src/test/java/com/amalitech/eurekaserver/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.amalitech.discoveryserver;
+package com.amalitech.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryServerApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
